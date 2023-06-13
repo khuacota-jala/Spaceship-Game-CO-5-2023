905b17ad39c4ee84d2d1d7c4cd56e8f90d064b64
Jhonnier-Jala <Jhonnier.Rodriguez@fundacion-jala.org>
2023-06-13 13:13:30 -0500

    tarea 1

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -27,14 +27,17 @@ class Spaceship:
     def move_left(self):
         if self.rect.left > 0:
             self.rect.x -= 10
+        else:
+            self.rect.x = SCREEN_WIDTH - self.rect.width
     def move_right(self):
         if self.rect.right < SCREEN_WIDTH:
             self.rect.x += 10
+        else:
+            self.rect.x = 0
     def move_up(self):
         if self.rect.y > (SCREEN_HEIGHT // 2) - 40:
             self.rect.y -= 10
     def move_down(self):
         if self.rect.y < (SCREEN_WIDTH // 2) - 60:
             self.rect.y += 10
-    
     
