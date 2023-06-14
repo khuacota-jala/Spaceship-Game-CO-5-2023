c49eb1b4e256b26125f3712503214725a7a3e713
Jhonnier-Jala <Jhonnier.Rodriguez@fundacion-jala.org>
2023-06-13 20:23:06 -0500

    agregar otro ship y mostrar las naves aleatoriamente cada cierto tiempo

--- game/components/enemies/enemy_handler.py
+++ game/components/enemies/enemy_handler.py
@@ -4,8 +4,14 @@ class EnemyHandler:
     def __init__(self):
         self.enemies = []
         self.enemies.append(Ship())
+        self.timer = 0
+        self.delay = 200
     
     def update(self):
+        self.timer += 1
+        if self.timer >= self.delay:
+            self.enemies.append(Ship())
+            self.timer = 0
         for enemy in self.enemies:
             enemy.update()
 
--- game/components/enemies/ship.py
+++ game/components/enemies/ship.py
@@ -1,13 +1,15 @@
 import pygame
-
+import random
 from game.components.enemies.enemy import Enemy
-from game.utils.constants import ENEMY_1
+from game.utils.constants import ENEMY_1,ENEMY_2
 
 class Ship(Enemy):
     WIDHT = 40
     HEIGHT = 60
+    
 
     def __init__(self):
-        self.image = ENEMY_1
+        self.image = [ENEMY_1, ENEMY_2]
+        self.image = random.choice(self.image)
         self.image = pygame.transform.scale(self.image,(self.WIDHT,self.HEIGHT))
         super().__init__(self.image)
